65f6478d3339da9afbc30f33c5f1adf1cf0fae5c
devashish <devashish@cse.iitk.ac.in>
2015-06-05 17:10:52 +0530

    Added socket.io chat code.

--- server.js
+++ server.js
@@ -5,6 +5,31 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public/'));
 
+io.on('connection', function(socket){
+   console.log('a user connected');
+  
+   socket.on('disconnect', function(){
+      console.log('user disconnectedfrom room: %s' , socket.room);
+      socket.leave(socket.room);
+    });
+
+    socket.on('addToRoom', function (roomName){
+      socket.room = roomName;
+      console.log(socket.room);
+      socket.join(roomName);
+
+      console.log('Added to room: %s',roomName);
+    });
+
+    socket.on('chatmsg', function(msg){
+      socket.broadcast.to(socket.room).emit('chatmsg', msg.msg);
+      console.log('%s: %s', socket.room, msg.msg);
+   });
+
+
+
+
+
 
 
 
