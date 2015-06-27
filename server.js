67624aec820d77ad66e6cbd3f60073529425f01b
devashish <devashish@cse.iitk.ac.in>
2015-06-28 00:20:54 +0530

    added node-cluster.

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+// Include the cluster module
+var cluster = require('cluster');
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -140,3 +166,6 @@ io.on('connection', function(socket){
 http.listen( (process.env.PORT || 5000), function(){
   console.log('listening on port: %s',5000 );
 });
+
+
+}
